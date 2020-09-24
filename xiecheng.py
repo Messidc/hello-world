e4255683aa656e7c9e28c3f1b3260b920141a886
Messidc <dengdanchaoren@gmail.com>
2020-09-24 08:02:36 +0800

    Update xiecheng.py

--- xiecheng.py
+++ xiecheng.py
@@ -1,33 +1,6 @@
-import time
-import tkinter
-import Tkinter.messagebox
+def gen():
+    for x in ["a", "b", "c"]:
+        yield x
 
-
-def download():
-    # 模拟下载任务需要花费10秒钟时间
-    time.sleep(10)
-    tkinter.messagebox.showinfo('提示', '下载完成!')
-
-
-def show_about():
-    tkinter.messagebox.showinfo('关于', '作者: 骆昊(v1.0)')
-
-
-def main():
-    top = tkinter.Tk()
-    top.title('单线程')
-    top.geometry('200x150')
-    top.wm_attributes('-topmost', True)
-
-    panel = tkinter.Frame(top)
-    button1 = tkinter.Button(panel, text='下载', command=download)
-    button1.pack(side='left')
-    button2 = tkinter.Button(panel, text='关于', command=show_about)
-    button2.pack(side='right')
-    panel.pack(side='bottom')
-
-    tkinter.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+for i in gen():
+    print(i)
